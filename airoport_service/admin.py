9d72e7cd5ec64435a282c22ba1124f70ba3f2c55
elKINTARO <fedotovpoliy@gmail.com>
2025-12-02 15:13:11 +0200

    Initial commit - add models, make migrations, register in admin panel

--- airoport_service/admin.py
+++ airoport_service/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Country, Airline, Airport, Airplane, Flight, Ticket
 
-# Register your models here.
+admin.site.register(Country)
+admin.site.register(Airline)
+admin.site.register(Airport)
+admin.site.register(Airplane)
+admin.site.register(Flight)
+admin.site.register(Ticket)
--- airoport_service/models.py
+++ airoport_service/models.py
@@ -1,3 +1,65 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+class Country(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+    def __str__(self):
+        return self.name
+    class Meta:
+        verbose_name_plural = "Countries"
+
+class Airline(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+    def __str__(self):
+        return self.name
+
+class Airport(models.Model):
+    name = models.CharField(max_length=50)
+    country = models.ForeignKey(Country, on_delete=models.CASCADE, related_name='airports') #країна одна, аєропортів багато
+    airlines = models.ForeignKey(Airline, on_delete=models.CASCADE)#мені ту мені
+    def __str__(self):
+        return f"{self.name} {self.country.name}"
+
+class Airplane(models.Model):
+    name = models.CharField(max_length=50)
+    airline = models.ForeignKey(Airline, on_delete=models.CASCADE, related_name='airplanes')#одна авіакомпанія, багато літаків
+    capacity = models.IntegerField(default=215)
+    def __str__(self):
+        return f"{self.name} {self.airline.name}"
+
+class Flight(models.Model):
+    class Status(models.TextChoices):
+        SCHEDULED = 'scheduled', 'Scheduled'
+        BOARDING = 'boarding', 'Boarding'
+        DEPARTED = 'departed', 'Departed'
+        DELAYED = 'delayed', 'Delayed'
+        CANCELLED = 'cancelled', 'Cancelled'
+    route_number = models.CharField(max_length=50)
+    airplane = models.ForeignKey(Airplane, on_delete=models.CASCADE, related_name='flights')#один літак, багато рейсів
+    departure_date = models.DateField()
+    arrival_date = models.DateField()
+    origin = models.ForeignKey(Airport, on_delete=models.CASCADE, related_name='departures')
+    destination = models.ForeignKey(Airport, on_delete=models.CASCADE, related_name='arrivals')
+
+    status = models.CharField(max_length=16, choices=Status.choices, default=Status.SCHEDULED)
+    def __str__(self):
+        return f"{self.route_number} {self.origin} -> {self.destination}"
+
+class Ticket(models.Model):
+    class Status(models.TextChoices):
+        BOOKED = 'booked', 'Booked'
+        CANCELLED = 'cancelled', 'Cancelled'
+        USED = 'used', 'Used'
+
+    flight = models.ForeignKey(Flight, on_delete=models.CASCADE, related_name='tickets')
+    passanger = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.PROTECT, related_name='tickets')
+    row = models.IntegerField()
+    seat = models.CharField(max_length=4)
+
+    status = models.CharField(max_length=16, choices=Status.choices, default=Status.BOOKED)
+    booking_time = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        unique_together = ('flight', 'row', 'seat')#унікальність місця
+    def __str__(self):
+        return f"Ticket for {self.flight} (Seat{self.row} {self.seat})"
